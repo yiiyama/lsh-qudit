b9f1a2242ac55e2cf8d45fa65117b1d78315de9c
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-03-13 03:42:32 +0900

    cirq-based validator was too slow in the end

---
+++ lsh_qudit/validator_jax.py
@@ -0,0 +1,113 @@
+# pylint: disable=unused-argument, missing-class-docstring
+"""Circuit validation against hermitian / unitary matrices."""
+from collections.abc import Sequence
+from typing import Optional
+import numpy as np
+import jax
+import jax.numpy as jnp
+from qiskit import QuantumCircuit
+from .utils import clean_array
+
+
+def circuit_unitary(
+    circuit: QuantumCircuit,
+    qutrits=(),
+    ancillae=(),
+    diagonal=False,
+    clean=True
+):
+    num_qubits = circuit.num_qubits
+    # Qubit to axis map
+    qubit_map = {qubit: i for i, qubit in enumerate(circuit.qubits[::-1])}
+    full_shape = tuple(3 if iq in qutrits else 2 for iq in range(num_qubits - 1, -1, -1))
+
+    unitary = jnp.eye(np.prod(full_shape), dtype=np.complex128).reshape(full_shape + full_shape)
+    for datum in circuit.data:
+        gate = datum.operation
+        if gate.name == 'barrier':
+            continue
+        axes = tuple(qubit_map[q] for q in datum.qubits[::-1])
+        nq = len(axes)
+        if nq == 1 and num_qubits - axes[0] - 1 in qutrits:
+            op_unitary = np.array(gate)
+            if op_unitary.shape[0] == 2:
+                tmp = np.eye(3, dtype=np.complex128)
+                tmp[:2, :2] = op_unitary
+                op_unitary = tmp
+            op_unitary = jnp.array(op_unitary)
+        else:
+            op_unitary = jnp.array(gate)
+
+        shape = tuple(full_shape[iq] for iq in axes)
+        op_unitary = op_unitary.reshape(shape + shape)
+        op_axes = list(range(nq, 2 * nq))
+        unitary = jnp.tensordot(op_unitary, unitary, (op_axes, axes))
+        unitary = jnp.moveaxis(unitary, list(range(nq)), axes)
+
+    if ancillae:
+        source = tuple(num_qubits - i - 1 for i in ancillae)
+        source += tuple(2 * num_qubits - i - 1 for i in ancillae)
+        num_anc_axes = len(ancillae) * 2
+        dest = tuple(range(num_anc_axes))
+        unitary = jnp.moveaxis(unitary, source, dest)
+        unitary = unitary[(0,) * num_anc_axes]
+        full_shape = tuple(full_shape[iq] for iq in range(num_qubits - 1, -1, -1)
+                           if iq not in ancillae)
+
+    full_dim = np.prod(full_shape)
+    unitary = unitary.reshape(full_dim, full_dim)
+    if diagonal:
+        unitary = jnp.diagonal(unitary)
+
+    if clean:
+        return clean_array(unitary)
+    return unitary
+
+
+def validate_circuit(
+    circuit: QuantumCircuit,
+    target: np.ndarray,
+    subspace: Optional[tuple[Sequence[int]]] = None,
+    qutrits=(),
+    ancillae=(),
+    exponentiate=True,
+    diagonal=False,
+    result_only=True
+):
+    unitary = circuit_unitary(circuit, qutrits=qutrits, ancillae=ancillae, clean=False)
+    return validate_unitary(unitary, target,
+                            subspace=subspace, exponentiate=exponentiate,
+                            diagonal=diagonal, result_only=result_only)
+
+
+def validate_unitary(
+    unitary: np.ndarray,
+    target: np.ndarray,
+    subspace: Sequence[int] = None,
+    exponentiate=True,
+    diagonal=False,
+    result_only=True
+):
+    target = jnp.asarray(target)
+    if exponentiate:
+        target = jax.scipy.linalg.expm(-1.j * target)
+    else:
+        target = target.copy()
+
+    if subspace is not None:
+        unitary = unitary[:, subspace]
+        target = target[:, subspace]
+
+    test = jnp.einsum('ij,ik->jk', unitary.conjugate(), target)
+    is_identity = bool(jnp.allclose(test * test[0, 0].conjugate(), jnp.eye(test.shape[0])))
+    if result_only:
+        return is_identity
+
+    if diagonal and subspace is None:
+        unitary = np.diagonal(unitary)
+        target = np.diagonal(target)
+
+    unitary = clean_array(unitary)
+    target = clean_array(target)
+
+    return is_identity, unitary, target
